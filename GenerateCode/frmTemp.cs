7b16290eecf7a43c6dae647193c331c806131f77
yong <2758318132@qq.com>
2020-07-19 11:33:26 +0800

    模板编辑

--- GenerateCode/frmTemp.cs
+++ GenerateCode/frmTemp.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Windows.Forms;
 using GenerateCode.Models;
 using Winform.Helpers;
@@ -32,7 +33,7 @@ namespace Winform
 
             var actionColumn = new DataGridViewActionButtonColumn
             {
-                DataPropertyName = "Id",
+                DataPropertyName = "FullName",
                 HeaderText = "操作",
                 Name = "ColAction",
                 ReadOnly = true,
@@ -48,15 +49,12 @@ namespace Winform
             //用户单击DataGridView“操作”列中的“查看”按钮。
             if (DataGridViewTwoButtonCell.IsLeftButtonClick(sender, e))
             {
-                var objectId = dgvIncome["ColAction", e.RowIndex].Value.ToString(); // 获取所要删除关联对象的主键。
-                var result = MessageBox.Show($"Id={objectId}的记录确认修改吗？", "确认", MessageBoxButtons.OKCancel,
+                var fileFullName = dgvIncome["ColAction", e.RowIndex].Value.ToString(); // 获取所要删除关联对象的主键。
+                var result = MessageBox.Show($"{fileFullName}的模板文件,确认修改吗？", "确认", MessageBoxButtons.OKCancel,
                     MessageBoxIcon.Question);
                 if (result == DialogResult.OK)
                 {
-                    var frm = new frmTempEdit { FileFullName = objectId };
-                    frm.ShowDialog(this);
-                    frm = null;
-                    BindDataGrid();
+                    OpenFile.Open(fileFullName);
                 }
             }
 
@@ -64,70 +62,27 @@ namespace Winform
             //用户单击DataGridView“操作”列中的“删除”按钮。
             if (DataGridViewTwoButtonCell.IsRightButtonClick(sender, e))
             {
-                var objectId = dgvIncome["ColAction", e.RowIndex].Value.ToString(); // 获取所要删除关联对象的主键。
-                var result = MessageBox.Show($"确认删除Id={objectId}的记录吗？", "确认", MessageBoxButtons.OKCancel,
+                var fileFullName = dgvIncome["ColAction", e.RowIndex].Value.ToString(); // 获取所要删除关联对象的主键。
+                var result = MessageBox.Show($"{fileFullName}的模板文件,确认删除吗？", "确认", MessageBoxButtons.OKCancel,
                     MessageBoxIcon.Question);
                 if (result == DialogResult.OK)
                 {
-                    long id;
-                    if (long.TryParse(objectId, out id))
-                    {
-                        BindDataGrid();
-                    }
-                    else
-                    {
-                        throw new Exception("Id类型错误！");
-                    }
+                    File.Delete(fileFullName);
+                    BindDataGrid();
                 }
-
-                //Delete.LoadDataById(objectId);                                       // 根据关联对象的主键，从数据库中获取信息。
-                //FormMain.LoadNewControl(Delete.Instance);                            // 载入该模块的删除信息界面至主窗体显示。
             }
         }
 
 
         private void BindDataGrid()
         {
-            pageBarIncome.DataControl = dgvIncome;
-            var startIndex = pageBarIncome.CurPage <= 0
-                ? 0
-                : (pageBarIncome.CurPage - 1) * pageBarIncome.PageSize;
+            var root = AppDomain.CurrentDomain.BaseDirectory + @"\Temps";
+            var rootDir = new DirectoryInfo(root);
+            if (!rootDir.Exists)
+                rootDir.Create();
 
-            //var searchDto = new IncomeSearchDto
-            //{
-            //    IsGetTotalCount = true,
-            //    Start = dtpStart.Value,
-            //    End = dtpEnd.Value,
-            //    PageSize = pageBarIncome.PageSize,
-            //    StartIndex = startIndex,
-            //    Keywords=txtKeywords.Text
-            //};
-
-            //if (_isIncomeingSearch)
-            //{
-            //    searchDto.Start = DateTime.Now.Date;
-            //    searchDto.End = DateTime.Now.Date;
-            //}
-
-            //if (_leaseContractId.HasValue)
-            //{
-            //    searchDto.LeaseContractId = _leaseContractId.Value;
-            //}
-
-            //var informations = _incomeService.Search(searchDto);
-            //pageBarIncome.DataSource = new PageData
-            //{
-            //    //当前页数，如果无记录，默认为1，否则按实际页面
-            //    PageCount =
-            //        searchDto.TotalCount > 0
-            //            ? (int)Math.Ceiling((double)searchDto.TotalCount / pageBarIncome.PageSize)
-            //            : 1,
-            //    CurPage = pageBarIncome.CurPage,
-            //    PageSize = pageBarIncome.PageSize,
-            //    PageList = informations,
-            //    RecordCount = searchDto.TotalCount
-            //};
-            pageBarIncome.DataBind();
+            var files = rootDir.GetFiles("*.temp");
+            dgvIncome.DataSource = files;
         }
 
         private void btnSearch_Click(object sender, EventArgs e)
@@ -160,7 +115,6 @@ namespace Winform
             dgvIncome.AutoSizeColumns(new List<string>() { "ColAction" });
         }
 
-        private int? _leaseContractId;
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
